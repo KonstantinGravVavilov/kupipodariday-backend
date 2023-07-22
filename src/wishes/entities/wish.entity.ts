da449277826558ea6d94fb8847e3c9af15edfab1
Konstantin Vavilov <gravekon111@yandex.ru>
2023-07-22 13:10:34 +0500

    feat: add entities

---
+++ src/wishes/entities/wish.entity.ts
@@ -0,0 +1,63 @@
+import { Length, IsUrl, IsInt, IsDecimal } from 'class-validator';
+import { Offer } from 'src/offers/entities/offer.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Wishlist } from 'src/wishlists/entities/wishlist.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  OneToMany,
+  ManyToMany,
+} from 'typeorm';
+
+@Entity()
+export class Wish {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  @Length(1, 250)
+  name: string;
+
+  @Column()
+  @IsUrl()
+  link: string;
+
+  @Column()
+  @IsUrl()
+  image: string;
+
+  @Column('numeric')
+  @IsDecimal({ decimal_digits: '2' })
+  price: number;
+
+  @Column('numeric')
+  @IsDecimal({ decimal_digits: '2' })
+  raised: number;
+
+  @ManyToOne(() => User, (user) => user.wishes)
+  owner: User;
+
+  @Column()
+  @Length(1, 1024)
+  description: string;
+
+  @OneToMany(() => Offer, (offer) => offer.item)
+  offers: Offer[];
+
+  @ManyToMany(() => Wishlist, (wishlist) => wishlist.items)
+  wishlists: Wishlist[];
+
+  @Column()
+  @IsInt()
+  copied: number;
+}
